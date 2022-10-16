a4dd7b4962145d03d720ce7111ae3defa3141b52
LFrakie <randymas15@hotmail.com>
2022-10-15 21:18:45 -0500

    beta changes /gitu-2

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -1,33 +1,21 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import { conn } from '../../utils/database';
+import { NextApiRequest, NextApiResponse } from "next";
+import { conn } from '../../utils/database';
 
 
-// export default async (req: NextApiRequest, res: NextApiResponse) => {
-//   const response = await conn.query("SELECT NOW()");
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const response = await conn.query("SELECT NOW()");
 
-//   console.log(response);
+  console.log(response);
   
-//   return res.json({ message: "pong" });
-// };
-
-import {NextApiRequest, NextApiResponse} from "next";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  return res.json({message: "pong"});
-}
+  return res.json({ message: "pong" });
+};
 
 
+// For tests
 
+// import {NextApiRequest, NextApiResponse} from "next";
 
-// import type { NextApiRequest, NextApiResponse } from 'next'
-
-// type Data = {
-//   name: string
+// export default (req: NextApiRequest, res: NextApiResponse) => {
+//   return res.json({message: "pong"});
 // }
 
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   res.status(200).json({ name: 'John Doe' })
-// }
--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,16 +1,15 @@
-import { Pool } from 'pg'
-let conn: any
+import { Pool } from 'pg';
+let conn: any;
 
 if (!conn){
-
 
 conn = new Pool({
     user: 'postgres',
     password: 'admin',
-    host: 'localhost',
+    host: '192.168.0.102',
     port: 5432,
     database: 'sigevi'
-})
+});
 }
 
 export { conn };
